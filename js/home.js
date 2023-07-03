d0433467f54fabb74839f4390d923b17b4bbab96
Patsamamiarivelo <myamhf8@gmail.com>
2023-07-03 22:53:09 +0300

    Update Home

--- js/home.js
+++ js/home.js
@@ -1,1 +1,25 @@
 
+/*  || SCROLL TO HEROS AND ENEMY 
+ */
+// Get buttons
+const hero = document.getElementById('hero');
+const nemesis = document.getElementById('nemesis');
+
+// Get destinations
+const batmanCinéma = document.getElementById('batman-cinéma');
+const batmanAdversaires = document.getElementById('batman-adversaires');
+
+// Functions scroll
+hero.addEventListener('click', ()=> {
+  window.scrollTo({
+    top: batmanCinéma.offsetTop,
+    behavior: 'smooth'
+  });
+})
+
+nemesis.addEventListener('click', ()=>{
+  window.scrollTo({
+    top: batmanAdversaires.offsetTop,
+    behavior: 'smooth'
+  })
+})
